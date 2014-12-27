3dee755b9a14a59b7e6620cb5670d509a4540f27
Vas <vasiliy.kvm@gmail.com>
2014-12-27 14:56:05 +0300

    Add utz 1 check_answer action

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ YTZ::Application.routes.draw do
   root 'utz#index'
 
   resources :test_utz_questions do
-    post :check_answers, on: :member
+    post :check_answer, on: :member
   end
 
   resources :matching_utz do
